78f6082b0eb0e50cd13dd694a95277db87d3bccf
Pedro Lucas <Pedro Lucas pedrolucaslopesmonteiro43@gmail.com>
2026-04-07 11:01:12 -0300

    main update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,56 @@
 import { getStatus } from "./services/statusService.js";
 import { renderStatus } from "./ui/statusUI.js";
 
+import { enviarImagem, listarAnalises } from "./services/analiseService.js";
+import { renderResultado, renderLista } from "./ui/analiseUI.js";
+
+// STATUS
 async function init() {
   try {
     const status = await getStatus();
     renderStatus(status);
-  } catch (error) {
-    document.getElementById("status").innerText = "Erro ao carregar status";
+  } catch {
+    console.log("Erro status");
   }
 }
 
 init();
+
+// CONVERTER IMAGEM → BASE64
+function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.split(",")[1]);
+    reader.onerror = error => reject(error);
+  });
+}
+
+// UPLOAD
+window.handleUpload = async function () {
+  const file = document.getElementById("fileInput").files[0];
+
+  if (!file) {
+    alert("Selecione uma imagem");
+    return;
+  }
+
+  const base64 = await toBase64(file);
+
+  try {
+    const result = await enviarImagem(base64);
+    renderResultado(result);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// LISTAR
+window.carregarLista = async function () {
+  try {
+    const lista = await listarAnalises();
+    renderLista(lista);
+  } catch (e) {
+    console.error(e);
+  }
+};
