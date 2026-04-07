7b5126f42f0d3066ac279cf8c30be45cc2cd516c
Pedro Lucas <Pedro Lucas pedrolucaslopesmonteiro43@gmail.com>
2026-04-07 11:34:47 -0300

    status update

--- js/ui/statusUI.js
+++ js/ui/statusUI.js
@@ -2,8 +2,7 @@ export function renderStatus(status) {
   const el = document.getElementById("status");
 
   el.innerHTML = `
-    <p><strong>API:</strong> ${status.status}</p>
-    <p><strong>IA:</strong> ${status.iaStatus}</p>
-    <p><strong>Timestamp:</strong> ${new Date(status.timestamp * 1000).toLocaleString()}</p>
+    <p><strong>Backend:</strong> ${status.backend}</p>
+    <p><strong>IA:</strong> ${status.iaService}</p>
   `;
 }
